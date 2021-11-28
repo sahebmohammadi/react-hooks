59970d7e23c24cad113b801e0af22a742f4f2e01
saheb mohammadi <saheb.put92@gmail.com>
2021-11-28 21:38:37 +0330

    add useMemo

---
+++ memo-hook/src/components/Counter.js
@@ -0,0 +1,37 @@
+import React, { useMemo, useState } from "react";
+
+// useMemo : useMemo will only recompute the memoized value
+//           when one of the dependencies has changed
+
+function Counter() {
+  const [counterOne, setCounterOne] = useState(0);
+  const [counterTwo, setCounterTwo] = useState(0);
+
+  const incrementOne = () => {
+    setCounterOne(counterOne + 1);
+  };
+
+  const incrementTwo = () => {
+    setCounterTwo(counterTwo + 1);
+  };
+
+  const isEvent = useMemo(() => {
+    let i = 0;
+    while (i < 2000000000) i++;
+    return counterOne % 2 === 0;
+  }, [counterOne]);
+
+  return (
+    <div>
+      <div>
+        <button onClick={incrementOne}>Count One - {counterOne}</button>
+        <span>{isEvent ? "Even" : "Odd"}</span>
+      </div>
+      <div>
+        <button onClick={incrementTwo}>Count Two - {counterTwo}</button>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
