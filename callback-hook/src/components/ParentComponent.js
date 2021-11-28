c8013be0223411ccaef25969a9624d5e51c9f5fd
saheb mohammadi <saheb.put92@gmail.com>
2021-11-28 21:24:39 +0330

    add useCallback hook

--- callback-hook/src/components/ParentComponent.js
+++ callback-hook/src/components/ParentComponent.js
@@ -1,19 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import Count from "./Count";
 import Button from "./Button";
 import Title from "./Title";
+
+// useCallback :  Returns a memoized callback.
+//            is  a hook that will return a memoized verison of the
+//            callback function that only changes if one of the dependencies
+//            has changed.
 
 function ParentComponent() {
   const [age, setAge] = useState(26);
   const [salary, setSalary] = useState(50000);
 
-  const incrementAge = () => {
+  const incrementAge = useCallback(() => {
     setAge(age + 1);
-  };
+  }, [age]);
 
-  const incrementSalary = () => {
+  const incrementSalary = useCallback(() => {
     setSalary(salary + 1000);
-  };
+  }, [salary]);
 
   return (
     <div>
